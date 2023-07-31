cbf3163d5d1f96ffc1ebdd8273197f449e70bcf5
Keorapetse Fani <Keorapetsefani2000@gmail.com>
2023-07-31 17:14:09 +0000

    updated the youtube tester to start reading from the file"

--- youtube_api_test.py
+++ youtube_api_test.py
@@ -2,10 +2,15 @@ from pytube import YouTube, Search, Stream, Channel
 
 from Amapiano import AmapionoYoutube
 
+with open('deep-house.txt', '+r') as f:
+    dj = [d.split('\n')[0] for d in f.readlines()]
+
+
 
 amapionoObj = AmapionoYoutube(
     # listOfDjs=["Romeo Makota", "Dj Maphorisa", "StankyDj",
     #            "MrJazzQ", "Kabza de small", "Mellow & Sleazy"]
-    listOfDjs=["Soulful amapiano mix"]
+    # listOfDjs=["Soulful amapiano mix"]
+    listOfDjs=dj
 )
 amapionoObj.initialize()
